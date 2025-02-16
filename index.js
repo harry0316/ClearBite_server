501b71829eb9115b02efbea96aaf727af5a2e54c
IKEDA HARUMI <harumimell@gmail.com>
2025-02-16 14:15:46 -0500

    add index and routes for setting table, and create get method for this

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+import userRoute from "./routes/user.js";
+import settingRoute from "./routes/setting.js";
+
+import "dotenv/config";
+const { PORT } = process.env;
+
+
+//accept all of port
+app.use(cors());
+app.use(express.json());
+
+//setting route for users 
+app.use("/api/users", userRoute);
+
+//setting route for setting 
+app.use("/api/setting", settingRoute);
+
+
+
+app.listen(PORT,()=>{
+console.log("confirm this server is working well", PORT);
+ });
