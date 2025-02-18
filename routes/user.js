cd6385497cb0824826b4e0bd000896a254184fd7
IKEDA HARUMI <harumimell@gmail.com>
2025-02-18 12:32:49 -0500

    add controllers/ routes for post/get users

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,16 @@
 import express from "express";
 const router = express.Router();
 
+import {
+addNewUser,getAllUsers
+} from "../controllers/userController.js"
+
+
+router.post("/",addNewUser)
+.get("/",getAllUsers);
+
+
+
 
 
 
